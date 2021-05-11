c97e0932e11699b09f7c2b06d8b46b4758216b60
MayurChawla <mr.mayurchawla@gmail.com>
2021-05-11 14:29:16 +0530

    added next pages feature

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const SEARCH_API =  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731
 function App() {
   const [movies_array, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
   useEffect(() => {
     GetMovies(FEATURED_API);
   }, []);
@@ -29,16 +30,31 @@ function App() {
   const onChangeHandler = (e) => {
     setSearchTerm(e.target.value);
   }
+  const prevPage = () => {
+    if(pageNumber >1)
+    {
+      const pg = pageNumber-1;
+      setPageNumber(pg);
+      GetMovies("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page="+pg);
+    }
+  }
+  const nextPage = () => {
+    const pg = pageNumber+1;
+    setPageNumber(pg);
+    GetMovies("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page="+pg);
+  }
+  const siteClicked = () => {
+    GetMovies(FEATURED_API);
+    setPageNumber(1);
+  }
   return (
     <>
-    
     <header>
-        <h2 className="site-name">My Movies</h2>
+        <h2 className="site-name"><span onClick={siteClicked}>My Movies</span></h2>
         <form action="" onSubmit={onSubmitHandler}>
           <input type="search" placeholder="search..." className="searchbar"
             value={searchTerm} onChange={onChangeHandler}
           ></input>
-
         </form>
       </header>
     <div className="movie-container">
@@ -47,10 +63,15 @@ function App() {
         <GetMovie key={movie.id} {...movie}/>
       ))}
     </div>
-    <footer>
-      <a> "prev" </a>
-          pagenumber
-      <a> "next" </a>
+    <div className="bars-to-pages">
+      <div className="movie-pages">
+        <button className="page-change" onClick={prevPage}>&lt;&nbsp;&lt;&nbsp;</button>
+            <span className="pgnumber">{pageNumber}</span>
+        <button className="page-change" onClick={nextPage}>&nbsp;&gt;&nbsp;&gt;</button>
+      </div>
+    </div>
+    <footer className="footer">
+      © 2021 All rights reserved
     </footer>
     </>
   );
